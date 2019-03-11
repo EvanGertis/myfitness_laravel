cd2e10dc64a2108b16ddd44a8d871dcd7c8f1b7d
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-11 15:37:47 -0400

    changed file name

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Laravel 5.7 & MySQL CRUD Tutorial</title>
+  <title>My Workouts</title>
   <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
 </head>
 <body>
